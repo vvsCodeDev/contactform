f9978070077a8e330b6cfe87004b45d12ceb6cd0
Justin <jcalhoun@happyfuncorp.com>
2022-06-07 12:16:44 -0700

    test

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // Initialize Firebase (ADD YOUR OWN DATA)
 var config = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx",
-  databaseURL: "xxxxx",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx",
-  messagingSenderId: "xxxxx"
+  apiKey: "AIzaSyDeCSSaaqBzGY7fADL8ndGAVzeJFmxAK6k",
+  authDomain: "vvscodeweb-c0453.firebaseapp.com",
+  projectId: "vvscodeweb-c0453",
+  storageBucket: "vvscodeweb-c0453.appspot.com",
+  messagingSenderId: "1041652245769",
+  appId: "1:1041652245769:web:9de367b7e3a9f968b55fd8",
+  measurementId: "G-ZHN3DBQS3J"
 };
-firebase.initializeApp(config);
 
-// Reference messages collection
-var messagesRef = firebase.database().ref('messages');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Reference contactInfo collections
+let contactInfo = firebase.database().ref("info");
 
 // Listen for form submit
 document.getElementById('contactForm').addEventListener('submit', submitForm);
@@ -18,13 +22,14 @@ document.getElementById('contactForm').addEventListener('submit', submitForm);
 // Submit form
 function submitForm(e){
   e.preventDefault();
+  console.log("email form submitted");
 
   // Get values
-  var name = getInputVal('name');
-  var company = getInputVal('company');
-  var email = getInputVal('email');
-  var phone = getInputVal('phone');
-  var message = getInputVal('message');
+  let name = getInputVal('name');
+  let company = getInputVal('company');
+  let email = getInputVal('email');
+  let phone = getInputVal('phone');
+  let message = getInputVal('message');
 
   // Save message
   saveMessage(name, company, email, phone, message);
