b9f817ce6b1bdd6c9df03b156505a1046555de34
Justin <jcalhoun@happyfuncorp.com>
2022-07-23 09:44:51 -0700

    test

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js";
 
 const firefbaseConfig = {
     apiKey: "AIzaSyBRt0y72nHzAmpQeK0EAZjW7zNBGPHq1ow",
